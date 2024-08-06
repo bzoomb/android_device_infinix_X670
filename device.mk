7602e00224c9cbef2b8a57d49a40be4c8e372bc7
bzoomb <bzoomb1@gmail.com>
2024-08-06 21:01:49 +0700

    build sensor

--- device.mk
+++ device.mk
@@ -309,7 +309,12 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@2.0.vendor
 
-
+# Sensors
+PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.frameworks.sensorservice@1.0 \
+    android.hardware.sensors-service.multihal \
+    libsensorndkbridge
 
 
 
